383c995ca786eb091f289589ec222bfe572ead3a
gholly48 <gholly48@outlook.com>
2025-02-24 02:39:26 +0330

    Update 1

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="min-h-screen bg-gray-100">
         <title>لیست مشاغل</title>
       <main className="flex flex-col items-center justify-center py-10">
-        <h1 className="text-4xl font-bold text-blue-600">لیست مشاغل</h1>
+        <h1 className="text-4xl font-sans text-blue-600">لیست مشاغل</h1>
         <p className="text-gray-700 mt-2">پیدا کن، انتخاب کن، استخدام کن! 🚀</p>
       </main>
     </div>
